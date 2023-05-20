0352b252074ab4ace6161c64fd6cf1030fdedae9
aslihancm <comu.aslihan@gmail.com>
2023-05-20 21:48:23 +0300

    Proje Katmanları || Entities

---
+++ TarifDefrerim.Entity/Liked.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TarifDefrerim.Entity
+{
+    public class Liked
+    {
+        public int Id { get; set; }
+        public virtual TarifUser Owner { get; set; }
+        public virtual TarifUser LikedUser { get; set; }
+
+
+    }
+}
---
+++ TarifDefrerim.Entity/Note.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TarifDefrerim.Entity
+{
+    public class Note:MyEntityBase
+    {
+        public string Name { get; set; }
+        public string Text { get; set; }
+        public bool IsDraft { get; set; }
+        public int LikeCount { get; set; }
+        public virtual Category Category { get; set; }
+        public virtual TarifUser Owner { get; set; }
+        public virtual List< Comment> Comments{ get; set; }
+        public virtual List< Liked> Likes{ get; set; }
+
+
+
+
+    }
+}
