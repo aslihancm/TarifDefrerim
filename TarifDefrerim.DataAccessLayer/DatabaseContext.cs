f7883810f9b186ab3566149d9bdb71d6045b3bb1
aslihancm <comu.aslihan@gmail.com>
2023-05-21 09:58:49 +0300

    DAL || Database Context

---
+++ TarifDefrerim.DataAccessLayer/DatabaseContext.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TarifDefrerim.Entity;
+
+namespace TarifDefrerim.DataAccessLayer
+{
+    internal class DatabaseContext:DbContext
+    {
+
+        public DatabaseContext():base("NOTESQL")
+        {
+
+        }
+        public DbSet<TarifUser> Users { get; set; }
+        public DbSet<Note> Note { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+        public DbSet<Liked> Likeds { get; set; }
+
+    }
+}
